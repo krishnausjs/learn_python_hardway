011e05a9e59f19ebd2c5cee2429eb9da7e69a618
Murali Krishna Marimekala <murali.marimekala@gmail.com>
2017-12-18 22:24:20 -0800

    Adding ext3 and ext4 files that are used to learn variables in python
    Signed-off-by: Murali Krishna Marimekala <murali.marimekala@gmail.com>

---
+++ ex3.py
@@ -0,0 +1,14 @@
+print "I will count my chickens"
+
+print "Hens" , 25+34/6
+print "3%4",3%4
+print "25*3%4",25-3%4
+print "Roosters", 100-25*3%4
+print "Now I will count eggs"
+
+print 3+2+1-5+4%2-1/4+6
+
+print "Is it true that 3+2 < 5 -7 ?"
+print 3+3 < 5-7
+
+print "What is 3+2?", 3+2
